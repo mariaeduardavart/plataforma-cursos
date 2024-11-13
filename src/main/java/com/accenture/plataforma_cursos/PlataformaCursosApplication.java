21292c982fb357ebbe6ba6642df3868153b61720
mariaeduardavart <dudavart@gmail.com>
2024-11-13 15:23:40 -0300

    initial commit

---
+++ src/main/java/com/accenture/plataforma_cursos/PlataformaCursosApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.plataforma_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaCursosApplication.class, args);
+	}
+
+}
